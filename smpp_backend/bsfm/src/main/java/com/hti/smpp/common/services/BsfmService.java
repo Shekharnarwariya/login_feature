9ff810ca189d4f17f7e032764fd60d16886db9de
VinodYogi6263 <rajguruv737@gmail.com>
2023-11-29 16:12:30 +0530

    resolve cros error

--- smpp_backend/bsfm/src/main/java/com/hti/smpp/common/services/BsfmService.java
+++ smpp_backend/bsfm/src/main/java/com/hti/smpp/common/services/BsfmService.java
@@ -1,17 +1,14 @@
 package com.hti.smpp.common.services;
 
-import java.util.List;
-
-import com.hti.smpp.common.bsfm.dto.Bsfm;
-import com.hti.smpp.common.dto.BsfmDto;
+import com.hti.smpp.common.dto.BsfmFilterFrom;
 
 
 public interface BsfmService {
 	
-	public void addBsfmProfile(BsfmDto bsfm, String username) throws Exception;
-	public List<Bsfm> showBsfmProfile(String masterId);
-	public void updateBsfmProfile(BsfmDto bsfm, String username) throws Exception;
-	public void deleteBsfmActiveProfile(String profilename) throws Exception;
-	public void bsfmDeleteProfile() throws Exception;
-	public boolean updateBsfmProfileFlag(String flag)throws Exception;
+	public void addBsfmProfile(BsfmFilterFrom bsfmFilterFrom, String username) throws Exception;
+//	public List<Bsfm> showBsfmProfile(String masterId);
+//	public void updateBsfmProfile(BsfmDto bsfm, String username) throws Exception;
+//	public void deleteBsfmActiveProfile(String profilename) throws Exception;
+//	public void bsfmDeleteProfile() throws Exception;
+//	public boolean updateBsfmProfileFlag(String flag)throws Exception;
 }
