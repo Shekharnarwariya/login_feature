ebd79293b3fe2667a4e42e9090773f585c135c1c
om-kanthariya <119924838+om-kanthariya@users.noreply.github.com>
2024-01-10 15:06:12 +0530

    Smpp Dlt Service (#33)
    * DLT Updated Code Module
    
    * DLT service module
    
    * Request Changes in Dlt
    
    * Dlt Updates
    
    * Dlt Updated Code

--- smpp_backend/dlt/src/main/java/com/hti/smpp/common/request/DltTempRequest.java
+++ smpp_backend/dlt/src/main/java/com/hti/smpp/common/request/DltTempRequest.java
@@ -1,25 +1,37 @@
 package com.hti.smpp.common.request;
-
+//DltTempRequest represents a request object for DLT (Do Not Disturb) template entry
+//This class is a Plain Old Java Object (POJO) used for data transfer between client and server
 import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.validation.constraints.NotBlank;
 
 
 public class DltTempRequest {
 
+	// Unique identifier for the DLT template entry
 	private int id;
 
+	 // Content of the DLT template
+	 @NotBlank(message = "template is required")
 	private String template;
 
+	 // Identifier for the DLT template
+	 @NotBlank(message = "templateId is required")
 	private String templateId;
 
+	 // Identifier for the PE (Principal Entity) associated with the DLT template entry
+	 @NotBlank(message = "Principal ID is required")
 	private String peId;
 	
+	 // MultipartFile object representing the template file
 	private MultipartFile templateFile;
 
+	 // Default constructor
 	public DltTempRequest() {
 		
     }
 	
-	
+	// Parameterized constructor to initialize the DltTempRequest object with values
 	public DltTempRequest(int id, String template, String templateId, String peId, MultipartFile templateFile) {
 		super();
 		this.id = id;
@@ -29,43 +41,52 @@ public class DltTempRequest {
 		this.templateFile = templateFile;
 	}
 
+	 // Getter method for retrieving the MultipartFile representing the template file
 	public MultipartFile getTemplateFile() {
 		return templateFile;
 	}
 
+	 // Setter method for setting the MultipartFile representing the template file
 	public void setTemplateFile(MultipartFile templateFile) {
 		this.templateFile = templateFile;
 	}
 
-
+	// Getter method for retrieving the ID of the DLT template entry
 	public int getId() {
 		return id;
 	}
 
+	// Setter method for setting the ID of the DLT template entry
 	public void setId(int id) {
 		this.id = id;
 	}
 
+	 // Getter method for retrieving the content of the DLT template
 	public String getTemplate() {
 		return template;
 	}
 
+	 // Setter method for setting the content of the DLT template
 	public void setTemplate(String template) {
 		this.template = template;
 	}
 
+	// Getter method for retrieving the template ID
 	public String getTemplateId() {
 		return templateId;
 	}
 
+	// Setter method for setting the template ID
 	public void setTemplateId(String templateId) {
 		this.templateId = templateId;
 	}
 
+	// Getter method for retrieving the PE ID associated with the DLT template entry
 	public String getPeId() {
 		return peId;
 	}
 
+	 // Setter method for setting the PE ID associated with the DLT template entry
 	public void setPeId(String peId) {
 		this.peId = peId;
 	}
--- smpp_backend/dlt/src/main/java/com/hti/smpp/common/services/DltService.java
+++ smpp_backend/dlt/src/main/java/com/hti/smpp/common/services/DltService.java
@@ -5,35 +5,52 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
-import com.hti.smpp.common.config.dto.DltEntry;
-import com.hti.smpp.common.config.dto.DltTemplEntry;
+//
+//import com.hti.smpp.common.config.dto.DltEntry;
+//import com.hti.smpp.common.config.dto.DltTemplEntry;
 import com.hti.smpp.common.request.DltRequest;
 import com.hti.smpp.common.request.DltTempRequest;
+import com.hti.smpp.common.response.DltResponse;
+import com.hti.smpp.common.response.DltTempResponse;
+
+//DltService interface defines the contract for handling DLT (Do Not Disturb) operations
+//These operations include saving, updating, listing, and deleting DLT entries and templates
+//It also provides methods to retrieve a single DLT entry or template by ID
+//The interface is marked with the @Service annotation to indicate it as a service component
 
 
 @Service
 public interface DltService {
 
+	// Method to save a DLT entry
 	public ResponseEntity<?> saveDltEntry(DltRequest entry, String username);
 
+	 // Method to add a DLT template
 	public ResponseEntity<?> addDltTemplate(String entry , MultipartFile file, String username);
 	
-	public ResponseEntity<List<DltEntry>> listDltEntry(String username);
+	 // Method to list DLT entries
+	public ResponseEntity<List<DltResponse>> listDltEntry(String username);
 	
-	public ResponseEntity<List<DltTemplEntry>> listDltTemplate(String username);
+	// Method to list DLT templates
+	public ResponseEntity<List<DltTempResponse>> listDltTemplate(String username);
 
+	// Method to update a DLT entry
 	public ResponseEntity<?> updateDltEntry(DltRequest entry , String username);
 	
+	 // Method to update a DLT template
 	public ResponseEntity<?> updateDltTemplate(DltTempRequest entry , String username);
 
-	public void deleteDltEntry(DltEntry entry , String username);
+	// Method to delete a DLT entry
+	public ResponseEntity<?> deleteDltEntry(DltRequest entry , String username);
 	
-	public void deleteDltTemplate(DltTemplEntry entry , String username);
+	  // Method to delete a DLT template
+	public ResponseEntity<?> deleteDltTemplate(DltTempRequest entry , String username);
 
-	public DltEntry getDltEntry(int id , String username);
+	 // Method to get a single DLT entry by ID
+	public DltResponse getDltEntry(int id , String username);
 	
-	public DltTemplEntry getDltTemplate(int id , String username);
+	 // Method to get a single DLT template by ID
+	public DltTempResponse getDltTemplate(int id , String username);
 
 
 	
