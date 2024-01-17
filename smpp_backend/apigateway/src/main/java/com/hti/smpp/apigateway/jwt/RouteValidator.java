1daee650cfb12f8556c25319953716a989d2097c
VinodYogi6263 <rajguruv737@gmail.com>
2024-01-17 10:55:23 +0530

    add user forgot password

--- smpp_backend/apigateway/src/main/java/com/hti/smpp/apigateway/jwt/RouteValidator.java
+++ smpp_backend/apigateway/src/main/java/com/hti/smpp/apigateway/jwt/RouteValidator.java
@@ -9,7 +9,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class RouteValidator {
 
-	public static final List<String> openApiEndpoints = List.of("/login/jwt", "/eureka");
+	public static final List<String> openApiEndpoints = List.of("/login/jwt", "/eureka", "/login/sendOTP",
+			"/login/validateOtp","/login/forgotPassword");
 	public Predicate<ServerHttpRequest> isSecured = request -> openApiEndpoints.stream()
 			.anyMatch(uri -> request.getURI().getPath().contains(uri));
 
