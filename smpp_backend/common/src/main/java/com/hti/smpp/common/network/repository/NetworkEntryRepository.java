ed4bf2f05e35025d10c6793b3f47515c84bca5a7
VinodYogi6263 <rajguruv737@gmail.com>
2024-01-22 17:26:01 +0530

    change natwork

--- smpp_backend/common/src/main/java/com/hti/smpp/common/network/repository/NetworkEntryRepository.java
+++ smpp_backend/common/src/main/java/com/hti/smpp/common/network/repository/NetworkEntryRepository.java
@@ -17,25 +17,26 @@ public interface NetworkEntryRepository extends JpaRepository<NetworkEntry, Inte
 
 	@Query(value = "SELECT DISTINCT n.mcc, n.country FROM NetworkEntry n ORDER BY n.country")
 	List<Object[]> findDistinctCountries();
-	
+
 	List<NetworkEntry> findByMcc(String mcc);
-	
+
 	@Query("SELECT n.id FROM NetworkEntry n WHERE n.mcc = :mcc AND n.mnc IN :mncList")
-    List<Integer> checkDuplicateMccMnc(@Param("mcc") String mcc, @Param("mncList") List<String> mncList);
-	
+	List<Integer> checkDuplicateMccMnc(@Param("mcc") String mcc, @Param("mncList") List<String> mncList);
+
 	@Modifying
-    @Query("DELETE FROM NetworkEntry n WHERE n.id IN :ids")
-    void deleteByIdIn(@Param("ids") List<Integer> ids);
-	
-	@Query(value="SELECT * FROM network n WHERE n.cc LIKE :cc AND n.mcc LIKE :mcc AND n.mnc LIKE :mnc ORDER BY n.cc", nativeQuery = true)
-    List<NetworkEntry> findByCcAndMccAndMnc(@Param("cc") String cc, @Param("mcc") String mcc, @Param("mnc") String mnc);
-	
-	@Query(value="SELECT * FROM network n WHERE n.cc LIKE :cc AND n.mcc LIKE :mcc AND n.mnc LIKE :mnc ORDER BY n.cc", nativeQuery = true)
-    Page<NetworkEntry> findByPaginated(@Param("cc") String cc, @Param("mcc") String mcc, @Param("mnc") String mnc, Pageable pageable);
-	
-	@Query("SELECT DISTINCT n.mcc FROM NetworkEntry n WHERE n.cc LIKE :cc")
-    List<String> findDistinctMCCByCc(String cc);
-	
+	@Query("DELETE FROM NetworkEntry n WHERE n.id IN :ids")
+	void deleteByIdIn(@Param("ids") List<Integer> ids);
+
+	@Query(value = "SELECT * FROM network n WHERE n.cc LIKE :cc AND n.mcc LIKE :mcc AND n.mnc LIKE :mnc ORDER BY n.cc", nativeQuery = true)
+	List<NetworkEntry> findByCcAndMccAndMnc(@Param("cc") String cc, @Param("mcc") String mcc, @Param("mnc") String mnc);
+
+	@Query(value = "SELECT * FROM network n WHERE n.cc LIKE :cc AND n.mcc LIKE :mcc AND n.mnc LIKE :mnc ORDER BY n.cc", nativeQuery = true)
+	Page<NetworkEntry> findByPaginated(@Param("cc") String cc, @Param("mcc") String mcc, @Param("mnc") String mnc,
+			Pageable pageable);
+
+	@Query(value = "SELECT DISTINCT n.mcc FROM network n WHERE n.cc LIKE :cc", nativeQuery = true)
+	public List<String> findDistinctMCCByCc(String cc);
+
 	@Query("SELECT DISTINCT n.mnc, n.operator FROM NetworkEntry n WHERE n.mcc LIKE :MCC")
 	List<Object[]> findDistinctMNCAndOperatorByMCCLike(String MCC);
 
