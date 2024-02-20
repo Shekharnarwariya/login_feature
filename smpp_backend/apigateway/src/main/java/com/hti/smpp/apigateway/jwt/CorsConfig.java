aecb9e02833e84206bb09dfc7f77958c2dff07a2
VinodYogi6263 <rajguruv737@gmail.com>
2024-02-20 13:37:45 +0530

    resolved cros error

--- smpp_backend/apigateway/src/main/java/com/hti/smpp/apigateway/jwt/CorsConfig.java
+++ smpp_backend/apigateway/src/main/java/com/hti/smpp/apigateway/jwt/CorsConfig.java
@@ -16,6 +16,7 @@ public class CorsConfig {
 		 // Allow requests from "http://localhost:3000"
 		corsConfig.addAllowedOrigin("http://localhost:3000");
 		corsConfig.addAllowedOrigin("https://smpp.vercel.app");
+		corsConfig.addAllowedOrigin("http://5.32.122.147:3000/");
 		// Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
 		corsConfig.addAllowedMethod("*");
 		// Allow all headers
